fb79c92a13d92143c48f9d3f38a74b3723ce7e54
Stefan Liute <stefanliute@gmail.com>
2018-12-08 16:44:28 +0000

    Display rough JSON data in React component

---
+++ src/Stock.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+// import { mongoose } from '../server';
+
+class Stock extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  public componentDidMount() {
+    fetch('https://api.iextrading.com/1.0/stock/aapl/chart/1d')
+    .then(result => result.json())
+    .then(result => {
+      this.setState({ data: result });
+    })
+  }
+
+  public render() {
+    const stockData = Object.keys(this.state).map(i => this.state[i])[0];
+
+    return (
+      <div>
+        {stockData.map((item: any, i: any) => (
+          <li key={i}>
+            Open: {item.open} | Close: {item.close}
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Stock;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import Stock from './Stock';
+
 import './index.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <Stock />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
