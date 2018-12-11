4c6317431cdcb6dfaf904ede58906c6fb1d07cf5
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2018-12-11 11:45:18 +0000

    change to uri into props from state added more charts and formatting

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -3,31 +3,24 @@ import * as React from 'react';
 import { Line } from 'react-chartjs-2';
 
 
-export default class Chart extends React.Component<{}, {data: any, uri: any}> {
+export default class Chart extends React.Component<any> {
   constructor(props: any) {
     super(props);
     this.state = {
         data: [],
-        uri: 'https://api.iextrading.com/1.0/stock/aapl/chart/1m'
     };
   }
-  public componentWillReceiveProps(props: any){
-      this.setState({uri: props.uri})
-  }
-  
   public componentDidMount() {
-    fetch(this.state.uri)
-    // fetch(this.state.uri)
+    fetch(this.props.uri)
     .then(result => result.json())
     .then(result => {
       this.setState({ data: result });
     })
   }
-
   public render() {
     const stockData = Object.keys(this.state).map(i => this.state[i])[0];
     const chartData = stockData.map((item: any) => (item.open));
-    const labelData = chartData.map((item: any, i: any) => (i));
+    const labelData = stockData.map((item: any) => (item.label));
     const data = {
         datasets: [{
         borderColor: [
@@ -40,16 +33,13 @@ export default class Chart extends React.Component<{}, {data: any, uri: any}> {
         }],
         labels: labelData
     };
-
     const options: ChartOptions = {
       legend: {
         position: 'bottom',
       }
-     
     };
-
     return (
-     <div style={{height:'500px', width:'500px'}}>
+     <div style={{height:'300px', width:'500px'}}>
       <Line data={data} options={options}/> 
     </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,22 @@ import * as ReactDOM from 'react-dom';
 
 
 
+
 ReactDOM.render(
-  < Chart />,
-  document.getElementById('root') as HTMLElement
+  < Chart uri = {'https://api.iextrading.com/1.0/stock/aapl/chart/1m'}/>,
+  document.getElementById('first-chart') as HTMLElement
 );
 
 ReactDOM.render(
-  < Chart />,
-  document.getElementById('second') as HTMLElement
+  < Chart uri = {'https://api.iextrading.com/1.0/stock/aapl/chart/1d'}/>,
+  document.getElementById('second-chart') as HTMLElement
 );
+ReactDOM.render(
+  < Chart uri = {'https://api.iextrading.com/1.0/stock/aapl/chart/3m'}/>,
+  document.getElementById('third-chart') as HTMLElement
+);
+
+
+
+
 registerServiceWorker();
