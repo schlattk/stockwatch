a0d326218369a84eac324d019026bc0ce9db9e39
Stefan Liute <stefanliute@gmail.com>
2018-12-15 15:40:27 +0000

    Manually fix merge conflicts :(

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Stock from './Stock';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Stock />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Stock from './Stock';
-
-=======
-
 import Chart from './Graph';
->>>>>>> konrad
+
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-
-
-
 ReactDOM.render(
-<<<<<<< HEAD
-  <Stock />,
-  document.getElementById('root') as HTMLElement
-=======
   < Chart uri = {'https://api.iextrading.com/1.0/stock/aapl/chart/1m'}/>,
   document.getElementById('first-chart') as HTMLElement
->>>>>>> konrad
 );
 
 ReactDOM.render(
@@ -35,8 +19,5 @@ ReactDOM.render(
   < Chart uri = {'https://api.iextrading.com/1.0/stock/aapl/chart/3m'}/>,
   document.getElementById('third-chart') as HTMLElement
 );
-
-
-
 
 registerServiceWorker();
